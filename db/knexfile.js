0f34b82b9c4613f9e40f717abe19788bd01b4810
Jake Lewochko <jlewochko@gmail.com>
2018-11-17 20:57:50 -0500

    changed con

--- db/knexfile.js
+++ db/knexfile.js
@@ -3,23 +3,11 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: {
-      host: `ec2-50-19-249-121.compute-1.amazonaws.com`,
-      port: '5432',
-      user: 'vnzkjdgqnhyimo',
-      password: '6d7d790a69c39f416f1d6972702149a8307b56f5948e09de3731a077253f1616',
-      database: 'd312agjdj2q57l'
-    }
+    connection: 'postgres://qfahlytiadskfm:114dc1f043b529da069016c11d4d18bf27ae06b906f7515364c90d3194be5659@ec2-50-19-249-121.compute-1.amazonaws.com:5432/d312agjdj2q57l'
   },
   production: {
     client: 'pg',
-    connection: {
-      host: `ec2-50-19-249-121.compute-1.amazonaws.com`,
-      port: '5432',
-      user: 'vnzkjdgqnhyimo',
-      password: '6d7d790a69c39f416f1d6972702149a8307b56f5948e09de3731a077253f1616',
-      database: 'd312agjdj2q57l'
-    }
+    connection: 'postgres://qfahlytiadskfm:114dc1f043b529da069016c11d4d18bf27ae06b906f7515364c90d3194be5659@ec2-50-19-249-121.compute-1.amazonaws.com:5432/d312agjdj2q57l'
   },
 
   ssl: true,
